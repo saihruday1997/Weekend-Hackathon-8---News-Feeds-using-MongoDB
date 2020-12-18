ecd7ea6cf41d43297568b1debd8ced58cb382ca7
saihruday1997 <saihruday.08@gmail.com>
2020-12-18 22:19:35 +0530

    chngs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const newsArticleModel = require("./connector");
 
 const onePageArticleCount = 10
 
@@ -8,6 +9,12 @@ const onePageArticleCount = 10
 // Parse JSON bodies (as sent by API clients)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.get("/newFeeds", (req, res) => {
+    newsArticleModel.paginate({}, {offset: 0, limit: 10})
+        .then(result => res.send(result));
+})
+
 
 
 
